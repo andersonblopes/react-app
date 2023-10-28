290854cf691a285091a4b7bc8ec8ea22f4080769
Anderson Lopes <>
2023-10-28 19:54:22 +0200

    feat: Handles events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import {MouseEvent} from "react";
+
 function ListGroup() {
     const items = [
         'Fortaleza - CE',
@@ -6,12 +8,19 @@ function ListGroup() {
         'Natal - RN'
     ]
 
+    const handleClick = (event: MouseEvent) => console.log(event);
+
     return (
         <>
             <h1>List</h1>
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-                {items.map(item => (
-                    <li key={item}>{item}</li>)
+                {items.map((item) => (
+                    <li className="list-group-item"
+                        onClick={handleClick}
+                        key={item}>
+                        {item}
+                    </li>)
                 )}
             </ul>
         </>
